713ae0bf6425b4709f0085864626837e0c66f481
ante <an.blaskovic@gmail.com>
2024-09-05 22:22:33 +0200

    added filtering on teachers

---
+++ sql/tables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE events(
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  start_time TIMESTAMP NOT NULL,
+  end_time TIMESTAMP NOT NULL,
+  teacher_id INT REFERENCES teachers(id)
+
+);
+
+
+CREATE TABLE students(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	email VARCHAR(150) NOT NULL UNIQUE,
+	phone VARCHAR(150)
+
+
+);
+
+
+CREATE TABLE teachers(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(150) NOT NULL,
+	email VARCHAR(150),
+	phone VARCHAR(150)
+
+
+);
--- src/controllers/teacherController.js
+++ src/controllers/teacherController.js
@@ -1,8 +1,24 @@
 import db from "../config/database.js";
 
 const getTeacherPage = async (req, res) => {
+  const { searchByName, searchByEmail, searchByPhone } = req.query;
+  let queryParams = [];
+  let baseQuery = "SELECT * FROM teachers WHERE 1=1";
+
+  if (searchByName) {
+    queryParams.push(`%${searchByName}%`);
+    baseQuery += ` AND name ILIKE($${queryParams.length})`;
+  }
+  if (searchByEmail) {
+    queryParams.push(`%${searchByEmail}%`);
+    baseQuery += ` AND email ILIKE($${queryParams.length})`;
+  }
+  if (searchByPhone) {
+    queryParams.push(`%${searchByPhone}%`);
+    baseQuery += ` AND phone ILIKE($${queryParams.length})`;
+  }
   try {
-    const result = await db.query("SELECT * FROM teachers");
+    const result = await db.query(baseQuery, queryParams);
 
     res.render("teachers.ejs", { teachers: result.rows });
   } catch (err) {
